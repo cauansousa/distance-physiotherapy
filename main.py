26806bef96c4ac302897841b249009d090ac347a
Cauan Sousa <103227067+cauansousa@users.noreply.github.com>
2022-09-13 10:35:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
+exercise = input("qual ex deseja fazer?")
+
 def calculate_angle(a,b,c):
     a = np.array(a) # First
     b = np.array(b) # Mid
@@ -53,44 +55,90 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
             right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
             
+            left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+            left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+            right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+            right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+            #left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            #left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            #left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+
             # Calculate angle
             left_elbow_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
             right_elbow_angle = calculate_angle(right_shoulder, right_elbow, right_wrist)
+            left_knee_angle = calculate_angle(left_hip, left_knee, left_ankle)
+            right_knee_angle = calculate_angle(right_hip, right_knee, right_ankle)
+            left_shoulder_angle = calculate_angle(left_hip, left_shoulder, left_elbow)
+            right_shoulder_angle = calculate_angle(right_hip, right_shoulder, right_elbow)
+
             
             # Visualize angle
             cv2.putText(image, str(int(left_elbow_angle)), 
                            tuple(np.multiply(left_elbow, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA
                                 )
             cv2.putText(image, str(int(right_elbow_angle)), 
                            tuple(np.multiply(right_elbow, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA
+                                )
+            cv2.putText(image, str(int(right_knee_angle)), 
+                           tuple(np.multiply(right_knee, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA
+                                )
+            cv2.putText(image, str(int(left_knee_angle)), 
+                           tuple(np.multiply(left_knee, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA
+                                )
+            cv2.putText(image, str(int(right_shoulder_angle)), 
+                           tuple(np.multiply(right_shoulder, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA
+                                )                           
+            cv2.putText(image, str(int(left_shoulder_angle)), 
+                           tuple(np.multiply(left_shoulder, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA
                                 )
             
             # Curl counter logic
-            if left_elbow_angle > 100 and right_elbow_angle > 100:
-                stage = "down"
-            if left_elbow_angle < 40 and right_elbow_angle < 40 and stage =='down':
-                stage="up"
-                counter +=1
-                print(counter)
+            if exercise == 'biceps':
+                if left_elbow_angle > 100 and right_elbow_angle > 100:
+                    stage = "down"
+                if left_elbow_angle < 40 and right_elbow_angle < 40 and stage =='down':
+                    stage="up"
+                    counter +=1
+
+            elif exercise == 'agachamento':
+                if left_knee_angle > 100 and right_knee_angle > 100 and stage == 'down':
+                    stage = 'up'
+                    counter += 1
+                if left_knee_angle < 90 and right_knee_angle < 90:
+                    stage = 'down'
+
+            elif exercise == 'desenvolvimento':
+                if left_shoulder_angle > 120 and right_shoulder_angle > 120 and stage == 'down':
+                    stage = 'up'
+                    counter += 1
+                if left_shoulder_angle < 120:
+                    stage = 'down'
                        
         except:
             pass
         
         # Render curl counter
         # Setup status box
-        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        #cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
         
         # Rep data
-        cv2.putText(image, 'REPS', (15,12), 
+        cv2.putText(image, 'COUNT', (15,12), 
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter), 
-                    (10,60), 
+        cv2.putText(image, str(counter), (10,60), 
                     cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
         
         # Stage data
-        cv2.putText(image, 'STAGE', (65,12), 
+        cv2.putText(image, 'STAGE', (80,12), 
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
         cv2.putText(image, stage, 
                     (60,60), 
